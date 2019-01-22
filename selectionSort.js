f9cb67403b6f4fc51881cad8fb9f2fbf546ff060
SparksFullStack <csparks@antioch.edu>
2019-01-22 14:29:40 -0800

    selection sort complete

--- selectionSort.js
+++ selectionSort.js
@@ -11,3 +11,18 @@ function findSmallest(arr){
 
     return smallestIndex;
 }
+
+function selectionSort(arr){
+    const sortedArr = [];
+    const length = arr.length;
+
+    for (let i = 0; i < length; i++){
+        let smallestIndex = findSmallest(arr);
+        let smallestElement = arr.splice(smallestIndex, 1);
+        sortedArr.push(smallestElement[0]);
+    }
+
+    return sortedArr;
+}
+
+console.log(selectionSort([4, 1, 6, 7, 8, 2, 5]));
